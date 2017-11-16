f38ac2058ebc2ffa118a58ed0739f4c0a5888439
VardenES <v.lopezarcas@gmail.com>
2017-11-16 18:45:36 +0100

    no message

--- index.php
+++ index.php
@@ -9,12 +9,11 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<!-- stylesheets -->
-	<link rel="stylesheet" href="css/font-awesome.css">
 	<link rel="stylesheet" href="css/style.css">
 	<!-- google fonts  -->
 	<link href="//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i" rel="stylesheet">
 	<link href="//fonts.googleapis.com/css?family=Josefin+Sans:300,400,400i,700" rel="stylesheet">
-
+	<!-- scripts -->
 <script>
 window.onload = function() {
   // dias is an object but you can think of it as a lookup table
